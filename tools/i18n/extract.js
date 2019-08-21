a595f72d697348a3bfc28c55e7ffd0b2aba3aa8c
Tustin2121 <tustin2121@gmail.com>
2019-08-21 01:49:29 -0400

    Update extract.js

--- tools/i18n/extract.js
+++ tools/i18n/extract.js
@@ -7,108 +7,110 @@ const OUTFILENAME = path.resolve(`../../gametext.i18n.txt`);
 
 let outfile = null;
 let index = 0;
-let currStr = '';
 
-const transformString = new Transform({
-	decodeStrings: false,
-	transform(chunk, encoding, next) {
-		let lastOff = 0;
-		// console.log(`chunk: ${typeof chunk}, ${chunk.constructor.name}, ${encoding}`);
-		chunk.replace(/([\s]*)?\.string "([^"]*)"$/, function(match, indent, str, offset){
-			if (lastOff + 4 > offset) { //
-				outfile.write(`${currStr}\n`);
-				index++;
-				currStr == '';
-			}
-			let ret = (!currStr)? `.i18n ${index}\n` : '';
-			currStr += str;
-			return ret;
-		});
-		next(null, chunk);
-	},
-});
-
-function sleep(delay) {
-	return new Promise((resolve)=>setTimeout(resolve, delay));
-}
-
-function renameFile(file) {
-	return new Promise(async (resolve, reject)=>{
-		let error = null;
-		for (let i = 0; i < 5; i++) {
-			try {
-				fs.renameSync(`${file}.1`, `${file}`);
-				return resolve();
-			} catch (err) {
-				error = err;
-				await sleep(200);
-			}
+function writeToOutput(line) {
+	return new Promise((resolve, reject)=>{
+		if (!outfile.write(`${line}\n`)) {
+			outfile.once('drain', resolve);
+		} else {
+			process.nextTick(resolve);
 		}
-		reject(err);
 	});
 }
 
-function processFile(file) {
-	return new Promise((resolve, reject)=>{
-		const filein  = fs.createReadStream(file, {encoding:'utf8', autoClose:true});
-		const fileout = fs.createWriteStream(`${file}.1`, {encoding:'utf8', autoClose:true});
+async function processFile(file) {
+	try {
+		let contents = fs.readFileSync(file, {encoding:'utf8'});
+		let lines = [];
+		let currStr = '';
+		let lastOff = 0;
+		let num = 0;
+		contents = contents.replace(/([\s]*)?\.string "([^"]*)"\n/gm, function(match, indent, str, offset){
+			if (lastOff + 4 < offset) { //
+				if (currStr) lines.push(currStr);
+				index++;
+				currStr = '';
+				lastOff = offset;
+				num++;
+			}
+			let ret = (!currStr)? `${indent}.i18n ${index}\n` : '';
+			currStr += str;
+			lastOff += match.length;
+			return ret;
+		});
+		if (currStr) {
+			lines.push(currStr);
+			num++;
+		}
+		if (lines.length) await writeToOutput(lines.join('\n'));
+		fs.writeFileSync(file, contents, {encoding:'utf8'});
+		console.log(`Replacing ${num} instances in ${file}`);
+	} catch (e) {
+		console.error(`Could not process file '${file}'.`);
+		console.error(e);
+	}
+}
+
+async function processDirectory(dir) {
+	const files = fs.readdirSync(dir, {withFileTypes:true});
+	for (const filename of files)
+	{
+		if (filename.startsWith('.')) continue;
+		const file = path.join(dir, filename);
+		if (file == OUTFILENAME) continue;
 		
-		fileout.on('finish', async ()=>{
-			try {
-				await renameFile(file);
+		let fileInfo = fs.statSync(file);
+		if (fileInfo.isFile())
+		{
+			switch (path.extname(file))
+			{
+				case '.inc':
+				case '.s':
+					await processFile(file);
+					break;
+				default:
+					break;
+			}
+		}
+		else if (fileInfo.isDirectory())
+		{
+			await processDirectory(file);
+		}
+	}
+}
+
+async function countIndex() {
+	return new Promise((resolve, reject)=>{
+		let rs = fs.createReadStream(OUTFILENAME, {flags:'r', encoding:'utf8'});
+		rs.on('error', (e)=>{
+			if (e.code == 'ENOENT') {
+				console.log('No current string file.');
 				resolve();
-			} catch (e) {
+			} else {
 				reject(e);
 			}
 		});
-		filein.on('error', (e)=>reject(e));
-		fileout.on('error', (e)=>reject(e));
-		
-		filein.pipe(transformString).pipe(fileout);
-	});
-}
-
-function processDirectory(dir) {
-	return new Promise(async (resolve, reject)=>{
-		try {
-			const files = fs.readdirSync(dir, {withFileTypes:true});
-			for (const filename of files)
-			{
-				if (filename.startsWith('.')) continue;
-				const file = path.join(dir, filename);
-				console.log(`Examining ${file}`)
-				let fileInfo = fs.statSync(file);
-				if (fileInfo.isFile())
-				{
-					switch (path.extname(file))
-					{
-						case '.inc':
-						case '.s':
-							await processFile(file);
-							break;
-						default:
-							break;
-					}
-				}
-				else if (fileInfo.isDirectory())
-				{
-					await processDirectory(file);
-				}
-			}
-		} catch (e) {
-			reject(e);
-		}
-	});
-}
-
-try {
-	fs.createReadStream(OUTFILENAME, {flags:'r', encoding:'utf8'})
-		.on('data', (chunk)=>{
+		rs.on('data', (chunk)=>{
 			index += chunk.split('\n').length - 1;
 		});
-} catch (e) {
-	console.log('No current string file.');
+		rs.on('finish', resolve);
+		resolve();
+	});
 }
 
-outfile = fs.createWriteStream(OUTFILENAME, {flags:'a', encoding:'utf8'});
-processDirectory(path.resolve('../../')).then(()=>console.log('Finished.')).catch((e)=>console.error(e));
+async function main() {
+	await countIndex();
+
+	try {
+		outfile = fs.createWriteStream(OUTFILENAME, {flags:'a', encoding:'utf8'});
+		outfile.on('error', (e)=>{ console.error(`ERROR: `, e); });
+		await processDirectory(path.resolve('../../'));
+		outfile.end();
+		console.log('Finished.');
+	} catch (e) {
+		console.error(`Error writing file.`);
+		console.error(e);
+	}
+}
+
+main();
